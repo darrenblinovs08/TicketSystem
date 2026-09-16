02408c5337e36bdd32d9f80caf5057f5f8b48d03
darrenblinovs08 <darrenblinovs08@gmail.com>
2026-09-16 12:53:16 +0100

    Add files via upload
    Added a class ticket which allows each ticket logged to be stored in the class.
    Will be adding close ticket , view ticket , edit ticket etc...

--- main.py
+++ main.py
@@ -1,24 +1,41 @@
 
+class Ticket():
+    def __init__(self, ticketid, category, description , priority,status):
+        self.ticketid = ticketid
+        self.category = category
+        self.description = description
+        self.priority = priority
+        self.status = "Open"
+
 
 def log_ticket():
     ticketID = 1
     print("||You have selected to log a ticket||")
     print(f"||Ticket number {ticketID} has been created||")
     issue = str(input("Please enter the description of the issue you would like to log: "))
+
+    priority =  ["Low", "Medium", "High" , "Critical"]
+    ticketPriority = priority[1]
+
     typesOfIssues  = ["Hardware", "Software", "Network", "Security", "User Accounts", "Email", "Data & Storage", "Performance", "Software Testing", "Automation"]
     for number , category in enumerate(typesOfIssues,1):
         print(f"{number}: {category}")
     categorySelection = int(input("Please enter the category you would like to use: "))
     categorySelection = categorySelection - 1
     print(f"Okay your issue has been logged as a {typesOfIssues[categorySelection]} error ")
-    ticket = {
-        "ticketID": ticketID,
-        "issue": issue,
-        "category": categorySelection,
-        "status": True
-    }
-    print(ticket)
 
+    ticket = Ticket(
+        ticketID,
+        typesOfIssues[categorySelection],
+        issue,
+        ticketPriority,
+        True
+    )
+    print(ticket.ticketid)
+    print(ticket.category)
+    print(ticket.description)
+    print(ticket.priority)
+    print(ticket.status)
 if __name__ == '__main__':
     log_ticket()
 
